5b87b54da77505a736917f808259700660bfb38d
osyoyu <osyoyu@osyoyu.com>
2021-03-24 01:00:14 +0900

    Superminimal recipe page

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+export const Header: FC = () => {
+  return (
+    <h1>Super cool recipe site</h1>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Header } from "../components/Header";
+
+const TopPage: FC = () => {
+  return <Header />;
+};
+
+export default TopPage;

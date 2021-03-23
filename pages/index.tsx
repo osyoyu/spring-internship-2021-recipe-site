aa30a822751eb72e2b5cb2d48af3cb25274cc554
osyoyu <osyoyu@osyoyu.com>
2021-03-24 02:31:51 +0900

    Proper routing for recipe page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,24 +20,6 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
 const TopPage: NextPage<Props> = ({ recipe }) => {
   return (
     <Layout>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.image_url} />
-      <hr />
-      <ul>
-        <li>作者: {recipe.author.user_name}</li>
-        <li>ひづけ: {recipe.published_at}</li>
-        <li>説明: {recipe.description}</li>
-      </ul>
-      <hr />
-      <h2>材料</h2>
-      <ul>
-        {recipe.ingredients.map(ingredient => <li>{ingredient.name} ({ingredient.quantity})</li>)}
-      </ul>
-      <hr />
-      <h2>手順</h2>
-      <ol>
-        {recipe.steps.map(step => <li>{step}</li>)}
-      </ol>
     </Layout>
   );
 };
---
+++ pages/recipes/[id].tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { Layout } from "../../components/Layout";
+
+import { Recipe } from "../../typing/Recipe";
+import { fetchRecipe } from "../../lib/apiClient";
+
+type Props = {
+  recipe: Recipe;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const recipeId = Number(context.query.id);
+  const recipe = await fetchRecipe(recipeId);
+  return {
+    props: {
+      recipe,
+    },
+  };
+};
+
+const TopPage: NextPage<Props> = ({ recipe }) => {
+  return (
+    <Layout>
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image_url} />
+      <hr />
+      <ul>
+        <li>作者: {recipe.author.user_name}</li>
+        <li>ひづけ: {recipe.published_at}</li>
+        <li>説明: {recipe.description}</li>
+      </ul>
+      <hr />
+      <h2>材料</h2>
+      <ul>
+        {recipe.ingredients.map(ingredient => <li>{ingredient.name} ({ingredient.quantity})</li>)}
+      </ul>
+      <hr />
+      <h2>手順</h2>
+      <ol>
+        {recipe.steps.map(step => <li>{step}</li>)}
+      </ol>
+    </Layout>
+  );
+};
+
+export default TopPage;
